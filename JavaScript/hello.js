ec3961fdf253bc8024c859c151594ce8216e4adb
biswajitrout45 <routbiswajit475@gmail.com>
2026-08-04 19:01:54 +0530

    js

---
+++ JavaScript/hello.js
@@ -0,0 +1,45 @@
+//Number
+let a=4;
+let b=4
+let c=7;
+console.log(a,b,c);
+console.log(typeof a, typeof b, typeof c);
+
+//String
+
+let n="Hello"
+let m="world"
+console.log(n,m)
+console.log(typeof n, typeof m)
+
+
+//Boolean
+
+let x=true
+let y=false
+console.log(x,y)
+console.log(typeof x, typeof y)
+
+//Undefined
+
+let p;
+console.log(p)
+console.log(typeof p)
+
+//null
+
+let q=null
+console.log(q)
+console.log(typeof q)
+
+//Symbol
+
+let r=Symbol("Hello")
+console.log(r)
+console.log(typeof r)
+
+//BigInt
+
+let s=BigInt(1234567890123456789012345678901234567890)
+console.log(s)
+console.log(typeof s)

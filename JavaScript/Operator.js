61dda54cf7e1c1d1ce4830c2e579d6b730d88bcb
biswajitrout45 <routbiswajit475@gmail.com>
2026-08-05 19:09:56 +0530

    JS

---
+++ JavaScript/Operator.js
@@ -0,0 +1,60 @@
+//Arithmetic Operators
+
+let x=5
+let y=7
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+console.log(x**y)
+
+//Assignment Operators
+
+let a=4
+
+console.log(a)
+console.log(a+=2)
+console.log(a-=2)
+console.log(a*=2)
+console.log(a/=2)
+console.log(a%=2)
+console.log(a**=2)
+
+//Comparison Operators
+
+let p=4
+let q=5
+
+console.log(p==q)
+console.log(p!=q)
+console.log(p===q)
+console.log(p!==q)
+console.log(p>q)
+console.log(p<q)
+console.log(p>=q)
+console.log(p<=q)
+
+//Logical Operators
+
+let d=true
+let b=false
+let c=true
+
+console.log(d && b)
+console.log(d && c)
+console.log(d || b)
+console.log(d || c)
+console.log(!d)
+console.log((d || c) && b)
+
+
+//unary Operators
+
+let t=5
+
+console.log(++t)
+console.log(--t)
+console.log(t++)
+console.log(t--)

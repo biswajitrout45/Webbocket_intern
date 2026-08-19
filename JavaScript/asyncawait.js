b1abf9277e356f66aa33beb2e16a757d2a69526c
biswajitrout45 <routbiswajit475@gmail.com>
2026-08-19 19:13:22 +0530

    js

---
+++ JavaScript/asyncawait.js
@@ -0,0 +1,39 @@
+// const getProducts = async () => {
+//     const res = await fetch('https://dummyjson.com/products');
+//     console.log(res);
+//     const data = await res.json();
+//     console.log(data.products)
+// }
+// getProducts()
+
+
+//Hoisting
+
+//variable Hoisting
+// console.log(x)
+// let x = 10
+// console.log(x)
+
+// console.log(y)
+
+// var y = 20
+
+// console.log(y)
+
+// conole.log(z)
+// const z = 20
+// console.log(z)
+//Function Hoisting
+// add(7,8)
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(5,6)
+
+
+// sub(20,5)
+// const sub = (a,b) => {
+//     console.log(a-b)
+// }
+// sub(10,5)
